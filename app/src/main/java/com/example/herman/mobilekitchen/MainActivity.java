cbb7686ebe10583617c77ee00b2a7b90d9b7f138
Anton Skudarnov <anybis-maat@mail.ru>
2018-04-08 18:43:47 +0300

    Product Add XML draft

--- app/src/main/java/com/example/herman/mobilekitchen/MainActivity.java
+++ app/src/main/java/com/example/herman/mobilekitchen/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.herman.mobilekitchen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -27,15 +28,18 @@ public class MainActivity extends AppCompatActivity
         addProductButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent intent = new Intent(MainActivity.this, AddProduct.class);
+                startActivity(intent);
+               //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                 //       .setAction("Action", null).show();
             }
         });
         FloatingActionButton deleteProductButton = (FloatingActionButton) findViewById(R.id.deleteProductButton);
         deleteProductButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "BLAAA", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
